2683cc5e99371b873188081cff8b7c0ae5204223
Patt91 <patrizio-guidi@hotmail.it>
2022-01-26 20:05:47 +0100

    Esercizio finale

---
+++ esercizio1.py
@@ -0,0 +1,11 @@
+
+print('this program is used to calculate the area of a room')
+print('________________')
+
+length = float(input('what is the length of room in meters?'))
+width = float(input('What is the width of room in meters?'))
+
+
+area=length*width
+print('the area of room is area' , area)
+ 
