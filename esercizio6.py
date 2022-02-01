839b8cb2f4f7ed91047c531e65df3dc3cb05e24f
Patt91 <patrizio-guidi@hotmail.it>
2022-02-01 20:18:14 +0100

    esercizio6

---
+++ esercizio6.py
@@ -0,0 +1,17 @@
+#this program reads a four-digit integer from the user and displays the sum of its digits
+
+number = int(input('insert a four-digit integer'))
+
+a=number%10
+number=int(number/10)
+b=number%10
+number=int(number/10)
+c=number%10
+number=int(number/10)
+d=number%10
+number=int(number/10)
+
+
+print(a+b+c+d)
+
+
