c95ef2389f91faa5053b12321bc0aa9d8a97f303
Patt91 <patrizio-guidi@hotmail.it>
2022-02-06 17:56:37 +0100

    esercizio 11

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 
-letters=str(input('input a letters'))
-
-vowel=[a,e,i,o,u]
-
-if letters in vowel:
-    print ('the letter is vowel')
+c4=261.63
+lista_c=[]
+x=0
+r=()
+while x <= 7:
+    r= c4 / 2**(4-x)
+    x += 1
+    lista_c.append(r)
+print (lista_c)
