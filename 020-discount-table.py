c0021f098132a061f80f40713c8a915084a833e5
Patt91 <patrizio-guidi@hotmail.it>
2022-02-14 19:00:12 +0100

    esercizio 20

---
+++ 020-discount-table.py
@@ -0,0 +1,15 @@
+
+perc=0.60
+
+art1=4.95
+art2=9.95
+art3=14.95
+art4=19.95
+art5=24.95
+
+
+print(f'the price is {art1} the discont is {round((art1*perc),2)} the total cost is {round(art1-(art1*perc),2)}')
+print(f'the price is {art2} the discont is {round((art2*perc),2)} the total cost is {round(art2-(art2*perc),2)}')
+print(f'the price is {art3} the discont is {round((art3*perc),2)} the total cost is {round(art3-(art3*perc),2)}')
+print(f'the price is {art4} the discont is {round((art4*perc),2)} the total cost is {round(art4-(art4*perc),2)}')
+print(f'the price is {art5} the discont is {round((art5*perc),2)} the total cost is {round(art5-(art5*perc),2)}')
