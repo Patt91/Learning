94b047a5d20a822185010d044c7675f5e698a019
Patt91 <patrizio-guidi@hotmail.it>
2022-03-31 20:03:13 +0200

    esercizio 45

---
+++ m1/045-magic-dates.py
@@ -0,0 +1,18 @@
+
+def magic ():
+    year=1901
+    day=1
+    month=1
+
+    while year != 2000:
+        month=1
+        while month < 12:
+            day=1
+            while day < 31:
+                if (day*month) == (year%100):
+                    print(f'{day, month, year} is a magic day')
+                day=day+1
+            month=month+1
+        year=year+1
+
+magic()
--- m1/test.py
+++ m1/test.py
@@ -1,39 +1,9 @@
 
-x='12cd'
 
-def hexadecimal(x):
-    esa=[]
-    nx=[]
-    indice=0
-    while indice < len(x):
-        nx.append((x[indice]))
-        indice=indice +1
-    for i in nx:
-        if i == 'A' or i == 'a':
-            esa.append(10)
-        elif i == 'B' or i== 'b':
-            esa.append(11)
-        elif i == 'C' or i == 'c':
-            esa.append(12)
-        elif i == 'D' or i == 'd':
-            esa.append(13)
-        elif i == 'E' or i == 'e':
-            esa.append(14)
-        elif i == 'F' or i == 'f':
-            esa.append(15)
-        else:
-            esa.append(int(i))
-    esa.reverse()
-    indice=0
-    c=int()
-    nesa=[]
-    for i in esa:
-        c = i * 16**indice
-        nesa.append(c)
-        indice=indice+1    
-    r=sum(nesa)
-    print(r)
+day=1
+month=1
+year=1992
 
-hexadecimal(x)
-
-y=input('insert the measure meter; tablespoons, teaspoons, cup \n')
+while year != 2000:
+    print('ok')
+    year=year+1
