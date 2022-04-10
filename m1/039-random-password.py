a0c4115bd7bcb6c5d047db915c3dcf9d7416c6c2
Patt91 <patrizio-guidi@hotmail.it>
2022-04-10 15:41:12 +0200

    correzione esercizio 39

--- m1/039-random-password.py
+++ m1/039-random-password.py
@@ -1,14 +1,11 @@
-import random
-
-n=random.randint(7,10)
-
-pas=[]
-
-for i in range(n):
-     pas.append(chr(random.randint(33,126)))
 
 
-print("".join(pas))
+def random_p ():
+     import random
+     n=random.randint(7,10)
+     password=[]
+     for p in range(n):
+          password.append(chr(random.randint(33,126)))
+     print("".join(password))
 
-    
-
+random_p()
