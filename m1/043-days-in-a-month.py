4fb9bec94c9604dc9732267eb99bbf49983258f8
Patt91 <patrizio-guidi@hotmail.it>
2022-04-10 15:53:48 +0200

    correzione esercizio 43

--- m1/043-days-in-a-month.py
+++ m1/043-days-in-a-month.py
@@ -1,21 +1,18 @@
+#this funcion check it out the day of the month based of the year
+def check (month,year):
+    m_31=[1,3,5,7,8,10,12]
+    m_30=[4,6,9,11]
+    leap=0
+    if (year % 4) == 0:
+        leap=1
+    if month in m_31:
+        print('the month has 31 days')
+    elif month == 2:
+        if leap == 1:
+            print ('the month has 29 days')
+        else:
+            print ('the month has 28 days')
+    elif month in m_30:
+        print('the month has 30 days')
 
-anno=int(input('insert the year as XXXX \n'))
-mese=int(input('insent the number of the month as integer \n'))
-bisestile=0
-
-m_31=[1,3,5,7,8,10,12]
-m_30=[4,6,9,11]
-
-
-if (anno % 4) == 0:
-    bisestile=1
-
-if mese in m_31:
-    print('the month has 31 days')
-elif mese == 2:
-    if bisestile == 1:
-        print ('the month has 29 days')
-    else:
-        print ('the month has 28 days')
-elif mese in m_30:
-    print('the month has 30 days')
+check(12,1995)
