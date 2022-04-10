a133a8dc4fcd6760088ec16382cce68c47507764
Patt91 <patrizio-guidi@hotmail.it>
2022-04-10 15:47:02 +0200

    correzione esercizio 40

--- m1/040-password-validation.py
+++ m1/040-password-validation.py
@@ -1,10 +1,10 @@
 
+#this function check it out if the password is strong enough
 def check (x):
     from tabnanny import check
     num=str(any(chr.isdigit() for chr in x))
     upper=str(any(chr.isupper() for chr in x))
     lower=str(any(chr.islower() for chr in x))
-
     if num == 'True' and lower == 'True' and upper == 'True':
         print('the password is secure')
     else:
