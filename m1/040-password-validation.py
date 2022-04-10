1e4bbf60ddf53346e77eb15be37e2b5a31283fec
Patt91 <patrizio-guidi@hotmail.it>
2022-04-10 15:45:30 +0200

    correzione esercizio 40

--- m1/040-password-validation.py
+++ m1/040-password-validation.py
@@ -1,16 +1,13 @@
 
-from tabnanny import check
+def check (x):
+    from tabnanny import check
+    num=str(any(chr.isdigit() for chr in x))
+    upper=str(any(chr.isupper() for chr in x))
+    lower=str(any(chr.islower() for chr in x))
 
+    if num == 'True' and lower == 'True' and upper == 'True':
+        print('the password is secure')
+    else:
+        print('choose another password')
 
-x=(input('insert your password\n'))
-
-
-num=str(any(chr.isdigit() for chr in x))
-upper=str(any(chr.isupper() for chr in x))
-lower=str(any(chr.islower() for chr in x))
-
-if num == 'True' and lower == 'True' and upper == 'True':
-    print('the password is secure')
-else:
-    print('choose another password')
-
+check(x=(input('insert your password\n')))
