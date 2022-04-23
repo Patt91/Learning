605325c7e6a9cfa21783dccbfa718480ca50eb82
Patt91 <patrizio-guidi@hotmail.it>
2022-04-23 18:00:38 +0200

    esercizio 11

--- m1/test2.py
+++ m1/test2.py
@@ -1,4 +1,35 @@
 
-import numpy as np
+
+from cmath import phase
+from http.client import parse_headers
 
 
+x=input('insert a sentence for a translation in Latinpig\n')
+vowels = "aeiou"
+consonant = "bcdfghjklmnpqrstvwxyz"
+phrase=x.split(' ')
+phrase_final=[]
+useful=[]
+
+i=0
+for word in phrase:
+    if word[0] in vowels:
+        add=word+'way'
+        phrase_final.append(add)
+    else:
+        useful=[]
+        add=()
+        for word in word:
+            useful.append(word)
+        while i <= len(word):
+            if useful[0] in consonant:
+                add=useful[0]
+                useful.append(add)
+                useful.pop(0)
+            i=i+1
+        for z in useful:
+            phrase_final.append(z)
+        
+    phrase_final.append(' ')
+             
+print("".join(phrase_final))
